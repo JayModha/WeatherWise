04d99980e5d4a130cbe9c3d56ac4cab35b7bbe80
JayModha <jaymodha10404@gmail.com>
2025-06-01 02:11:32 +0530

    Added some styling and created server

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const port=3000;
+const app= express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs");
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening to Port ${port}`);
+});
+
